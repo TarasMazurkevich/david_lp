2693b2f77cdc3178f9eca2deaddb4cac48a66927
Taras <tvsy6520@gmail.com>
2018-02-20 23:58:43 +0200

    create file structure, and create loader block : tvsy6520@gmal

---
+++ source/js/main.js
@@ -0,0 +1,7 @@
+// Preloader
+// $(window).on('load', function () {
+// 	var preloader = $('.loaderArea'),
+//     	loader = preloader.find('.loader');
+// 	loader.delay(1000).fadeOut('slow');
+// 	preloader.delay(1000).fadeOut('slow');
+// });
